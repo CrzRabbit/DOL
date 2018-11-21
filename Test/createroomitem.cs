8a1bebd2b4be22f231e2ff2dd822cc194e5d6c4d
CrzRabbit <33614042+CrzRabbit@users.noreply.github.com>
2018-11-21 22:40:02 +0800

    Add files via upload

---
+++ Test/createroomitem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createroomitem : MonoBehaviour {
+
+    public RectTransform item;
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < 5; i++)
+        {
+            GameObject.Instantiate(item).SetParent(transform);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
